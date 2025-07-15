45d8c63f96aebd3299486aeffd2ee4123776c050
Anandhbasha <anadhakm@gmail.com>
2025-07-15 20:28:18 +0530

    effect

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,29 +1,53 @@
+// // function App(){
+// //   const btnValues = ["Login","Register","Submit","Enter","Cancel"]
+// //   return(
+// //     <div className="app">
+// //       {btnValues.map((item)=>(
+// //         <button>{item}</button>
+// //       ))}
+// //     </div>
+// //   )
+// // }
+
+
+// // export default App
+
+// import Button from './Button'
+// import Login from './Login'
+
 // function App(){
 //   const btnValues = ["Login","Register","Submit","Enter","Cancel"]
 //   return(
 //     <div className="app">
 //       {btnValues.map((item)=>(
-//         <button>{item}</button>
+//         <Button value ={item}/>
 //       ))}
+//       <Login/>
 //     </div>
 //   )
 // }
-
-
 // export default App
+import React, { useEffect, useState } from 'react'
 
-import Button from './Button'
-import Login from './Login'
-
-function App(){
-  const btnValues = ["Login","Register","Submit","Enter","Cancel"]
-  return(
-    <div className="app">
-      {btnValues.map((item)=>(
-        <Button value ={item}/>
-      ))}
-      <Login/>
+const App = () => {
+  let [count,setCount] = useState(0)
+  let [bg,setBg] = useState("dark")
+  const handleChange = ()=>{
+    setCount(++count)
+    console.log(count);    
+  }
+  useEffect(()=>{
+    setBg(bg==="light"?"dark":"light")
+  },[count])
+  const bgChange=()=>{
+    
+  }
+  return (
+    <div className='App'>
+      <button onClick={handleChange}>Count{count}</button>
+      <button onClick={bgChange} style={{backgroundColor:bg==="dark"?"black":"white",color:bg==="dark"?"white":"black"}}>{bg}</button>
     </div>
   )
 }
+
 export default App
