abe6bc084036eb06a4d2c4b48e2d6a0d71495113
Anandhbasha <anadhakm@gmail.com>
2025-09-22 20:44:20 +0530

    python basic

---
+++ Python/index.py
@@ -0,0 +1,27 @@
+print("Hello Python")
+
+# simple datatypes
+a=10
+a=10.5
+name="arun"
+todayClass = True
+print(a)
+
+# complexdatatypes
+# list
+arr=[10,20,50,60]
+print(type(arr))
+print(arr[0])
+# tuple
+newList = (101,102,103,104)
+# set
+newSet = {101,101,102,105,103,104}
+print(newSet)
+# dictionary
+person = {
+    "names":"xyy",
+    "age":50,
+    "city":"CBE"
+}
+print(person['names'])
+
